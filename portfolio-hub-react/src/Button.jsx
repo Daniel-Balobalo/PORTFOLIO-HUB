28193112f08bc377b9c56b3b1084473f161f1efd
Daniel Balobalo <70095157+Daniel-Balobalo@users.noreply.github.com>
2025-03-18 09:27:20 +0800

    3/18/2025 Commit

--- portfolio-hub-react/src/Button.jsx
+++ portfolio-hub-react/src/Button.jsx
@@ -1,7 +1,11 @@
 
 function Button() {
     return (
+        <>
+        <h2>WELCOME TO DANIEL'S PORTFOLIO HUB</h2>
+        <h2></h2>
         <button class="enter-button" onclick="location.href='#'">Enter</button>
+        </>
     );
 }
 
